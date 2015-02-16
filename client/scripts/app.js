ca684f41972d1fc3c77409133e7508f01176f6df
dvandyke <haoled@gmail.com>
2015-02-16 15:30:04 -0800

    AJAX requests work and messages display

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,19 @@
-// YOUR CODE HERE:
+var temp = $.ajax({
+  type: "GET",
+  url: "https://api.parse.com/1/classes/chatterbox",
+  success: function(data){
+    var data = data;
+    displayMessages(data)
+  },
+  dataType: "json"
+});
 
+var displayMessages = function (data) {
+  console.log(data.results);
+  for(var i = 0; i<data.results.length; i++){
+    var user = data.results[i].username;
+    var message = data.results[i].text;
+    $("body").append('<div>'+user+": "+message+'</div>');
+    console.log('working');
+  }
+}
